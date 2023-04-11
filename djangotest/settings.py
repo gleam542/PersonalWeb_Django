c4778576e2d63f9fcfa947000ff0bf9ff35e0621
taiwinner.0246 <taiwinner.0246@gmail.com>
2023-04-11 15:36:42 +0800

    fix render mistake

--- djangotest/settings.py
+++ djangotest/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-0wr^=zh@u!1u4svh_1t@)o2jp(@r4s2mi$nh4$0&-a_1qe&p$t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
