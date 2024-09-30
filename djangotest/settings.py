15554afb53ec41b1fc86b37c116eaf386c813266
gleam542 <71612377+gleam542@users.noreply.github.com>
2024-10-01 03:52:19 +0800

    Update settings.py

--- djangotest/settings.py
+++ djangotest/settings.py
@@ -89,15 +89,12 @@ WSGI_APPLICATION = 'djangotest.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'example_db',  # MySQL 資料庫名稱
-        'USER': 'mick',        # MySQL 資料庫使用者
-        'PASSWORD': 'mick123',  # MySQL 使用者密碼
-  
-    }
+    'default': dj_database_url.config(
+        # Feel free to alter this value to suit your needs.
+        default= os.getenv('db'),
+        conn_max_age=600
+    )
 }
-
 
 
 # Password validation
